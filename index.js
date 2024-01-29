b084e5e738062da318c7937ce12f161293b7dbdb
Alex <a211019971420125@gmail.com>
2024-01-29 21:21:55 +0200

    added new features

--- index.js
+++ index.js
@@ -1,49 +1,58 @@
-const { default: axios } = require('axios');
 require('dotenv').config();
+const { default: axios } = require('axios');
 const TGBOT = require('node-telegram-bot-api');
 const token = process.env.TELEGRAM_BOT;
 const bot = new TGBOT(token, { polling: true });
 
-bot.on('message', async data => {
-  const text = data.text;
-  const chatId = data.chat.id;
-  const validText = 'к';
+const reqSet = new Set();
 
-  if (text === validText) {
-    axios
-      .get('https://api.unsplash.com/photos/random', {
-        params: {
-          client_id: process.env.CLIENT_ID,
-          query: 'funny_cat',
-          count: '1',
-        },
-      })
-      .then(res => {
-        const urlCat = res.data.map(item => {
-          return item.urls.small;
+bot.on('message', async data => {
+  const text = data.text.toLowerCase();
+  const chatId = data.chat.id;
+  const validText = 'Дай кота'.toLocaleLowerCase();
+
+  if (reqSet.has(chatId)) return;
+
+  switch (text) {
+    case validText:
+      axios
+        .get('https://api.unsplash.com/photos/random', {
+          params: {
+            client_id: process.env.CLIENT_ID,
+            query: 'funny_cats',
+            count: '1',
+          },
+        })
+        .then(res => {
+          const urlCat = res.data.map(item => {
+            return item.urls.small;
+          });
+          bot.sendPhoto(chatId, ...urlCat);
+          reqSet.add(chatId);
+          bot.sendMessage(chatId, 'ща ща').then(sentMessage => {
+            let timer = 5;
+            const intervalId = setInterval(() => {
+              timer--;
+              if (timer === 0) {
+                reqSet.delete(chatId);
+                clearInterval(intervalId);
+                bot.deleteMessage(chatId, sentMessage.message_id);
+              } else {
+                const newText = `Во, ля какой ${timer}`;
+                bot.editMessageText(newText, {
+                  chat_id: chatId,
+                  message_id: sentMessage.message_id,
+                });
+              }
+            }, 1000);
+          });
         });
-        bot.sendPhoto(chatId, ...urlCat);
-        bot.sendMessage(chatId, 'ща ща').then(sentMessage => {
-          let timer = 5;
-          const intervalId = setInterval(() => {
-            timer--;
-            if (timer === 0) {
-              clearInterval(intervalId);
-              bot.deleteMessage(chatId, sentMessage.message_id);
-            } else {
-              const newText = `ВО, ля какой ${timer}`;
-              bot.editMessageText(newText, {
-                chat_id: chatId,
-                message_id: sentMessage.message_id,
-              });
-            }
-          }, 1000);
-        });
-      });
-  } else {
-    return bot.sendMessage(
-      chatId,
-      `Напиши "${validText}" что бы получить котика`
-    );
+      break;
+    case '1488':
+      bot.sendMessage(chatId, process.env.SECRET_TOKEN); //Don't even think about it
+      break;
+    default:
+      bot.sendMessage(chatId, `Напишите "${validText}" что бы получить котика`);
+      break;
   }
 });
